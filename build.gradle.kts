76dddee48432e61b93ac89694c677e9c6475fdfe
ooney <62164814+ooneykabooey@users.noreply.github.com>
2026-01-17 17:05:34 -0500

    c.05_EA - Update to Hytale Version 4
    Replaced the HytaleServer.jar with the updated version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     compileOnly(files("libs/HytaleServer.jar"))
+    testCompileOnly(files("libs/HytaleServer.jar"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
