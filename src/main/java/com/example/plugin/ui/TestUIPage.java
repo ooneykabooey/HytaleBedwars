53d9e8d02076ad3af1ff4af4fcccabf2ca991bee
Yasha <yasha@wds4u.com>
2026-01-20 21:58:35 -0600

    c.12_EA - "Welcome" UI Frontend complete
    ### UI
    Gawly, ts got to be the most diabolical ui system i have ever graced my eyes upon. After like 2 hours of research i was able to get the FRONTEND done for the Welcome.ui ... SMH
    
    Atleast the other files should be done quicker, then i need to handle the back end for it (gonna slime someone out)
    
    I had to adjust some files for this to work btw
    
    ### Gradle
    
    I adjust the gradle so that it auto compiles the jar into my server mods folder cuz i had to restart every 2 seconds to see the changes. However i did not commit this cuz its a local path. If you would like to do it yourself you can add
    
    `
        doLast {
            copy {
                from(archiveFile.get().asFile)
                into(file(". . ./Server/mods"))
            }
        }
    
    `
    
    into the tasks.jar block.
    
    Enjoy.

--- src/main/java/com/example/plugin/ui/TestUIPage.java
+++ src/main/java/com/example/plugin/ui/TestUIPage.java
@@ -1,10 +1,14 @@
 package com.example.plugin.ui;
 
+import com.hypixel.hytale.codec.Codec;
+import com.hypixel.hytale.codec.KeyedCodec;
+import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.EntityEffect;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.Entity;
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
 import com.hypixel.hytale.server.core.ui.builder.EventData;
@@ -13,7 +17,26 @@ import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
-public class TestUIPage extends InteractiveCustomUIPage<GreetEventData> {
+
+public class TestUIPage extends InteractiveCustomUIPage<TestUIPage.GreetEventData> {
+
+    public static class GreetEventData {
+        public String playerName; // UI routes here
+
+        public static final BuilderCodec<GreetEventData> CODEC =
+                BuilderCodec.builder(GreetEventData.class, GreetEventData::new)
+                        .append(
+                                // "@PlayerName" = read from UI input (the @ prefix is important!)
+                                new KeyedCodec<>("@PlayerName", Codec.STRING),
+                                // Setter: put the value into obj.playerName
+                                (obj, val) -> obj.playerName = val,
+                                // Getter: read the value from obj.playerName
+                                obj -> obj.playerName
+                        )
+                        .add()
+                        .build();
+
+    }
 
     public TestUIPage(PlayerRef playerRef) {
         super(playerRef, CustomPageLifetime.CanDismissOrCloseThroughInteraction, GreetEventData.CODEC);
@@ -21,8 +44,14 @@ public class TestUIPage extends InteractiveCustomUIPage<GreetEventData> {
 
     @Override
     public void build(Ref<EntityStore> ref, UICommandBuilder cmd, UIEventBuilder evt, Store<EntityStore> store) {
-        cmd.append("Pages/Test.ui");
+        cmd.append("Pages/Welcome.ui");
 
-        evt.addEventBinding(CustomUIEventBindingType.Activating, "#GreetButton", new EventData().append("@PlayerName", "#NameInput.Value"));
+        evt.addEventBinding(CustomUIEventBindingType.Activating, "#WelcomeButtonsCancel", new EventData().append("@PlayerName", "#NameInput.Value"));
+    }
+
+    @Override
+    public void handleDataEvent(Ref<EntityStore> ref, Store<EntityStore> store, GreetEventData data) {
+        String name = data.playerName != null ? data.playerName : "Stranger";
+        playerRef.sendMessage(Message.raw("Hello, " + name + "!"));
     }
 }
