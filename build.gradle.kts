a9fafed1542cf35e52b163e9641309df71ea3de8
Yasha <yasha@wds4u.com>
2026-02-01 22:39:42 -0600

    Fixed everything plus had to redo the gradle (RIP ME)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        setUrl("https://www.cursemaven.com")
+    }
 }
 
 dependencies {
@@ -15,6 +19,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("curse.maven:hyui-1431415:7548594") // 0.5.10
 }
 
 tasks.test {
